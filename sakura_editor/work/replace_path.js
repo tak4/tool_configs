dacd0cf3a0ce1c784050353b54c27d003d1e595d
tak4 <tak.shi.4@gmail.com>
2025-07-29 05:35:53 +0900

    modify

--- sakura_editor/work/replace_path.js
+++ sakura_editor/work/replace_path.js
@@ -1,9 +1,25 @@
-// 全選択
+﻿// 全選択
 Editor.SelectAll()
 var select_string = Editor.GetSelectedString(0);
 
-// 正しいパスリテラルを記述（\\でエスケープ！）
-var raw_path = 'D:\\develop\\github\\tool_configs\\sakura_editor\\work\\sakura_grep';
+// 行ごとに分割
+var lines = select_string.split(/\r?\n/);
+
+// フォルダ行を取得する為の正規表現を作成
+var regex_folder_line = /^フォルダ +(.+)/;
+
+// 検索して該当する行だけを抽出
+var folder_line = "";
+for (var i = 0; i < lines.length; i++) {
+    if (regex_folder_line.test(lines[i])) {  // 正規表現でマッチするか確認
+        folder_line = lines[i];
+        break;
+    }
+}
+
+// フォルダ名のみ取得する
+var base_folder = folder_line.replace(regex_folder_line, '$1');
+MessageBox(base_folder);
 
 // 正規表現エスケープ関数
 function escapeRegExp(s) {
@@ -11,6 +27,6 @@ function escapeRegExp(s) {
 }
 
 // パスを全て削除
-var replaced = select_string.replace(new RegExp(escapeRegExp(raw_path), 'g'), '');
+var replaced = select_string.replace(new RegExp(escapeRegExp(base_folder), 'g'), '');
 
 MessageBox(replaced);
